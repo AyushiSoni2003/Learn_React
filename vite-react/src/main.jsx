6b574b331c78268ef95561050ae695c847871c0a
Ayushi Soni <ayushisoni31103@gmail.com>
2025-09-06 17:47:41 +0530

    create own react library

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function myApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+// This will work in react because react will convert this jsx to javascript object
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children : "click me to visit google"
+// };
+
+// This will work in react
+const anotherElement = <a href="https://www.google.com" target="_blank">Another link</a>
+
+// This will work in react
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://www.google.com', target: '_blank' },
+  'click me to visit google',
+  anotherElement // this is child element , we can have multiple child elements , ----evaluated expression , we can not write if condition here
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
